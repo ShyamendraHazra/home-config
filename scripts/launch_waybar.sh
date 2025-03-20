b9fb1080ca3878d29e21f79659ae2016c12586c9
Shyamendra Hazra <shyamendrahazra@archlinux.localdomain>
2025-03-20 09:44:36 +0530

    Update .gitignore and add launch_waybar.sh

---
+++ scripts/launch_waybar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Correctly reference the check_process_status.sh script
+CHECK_PROCESS_SCRIPT="$HOME/scripts/check_process_status.sh"
+
+# Example process to check
+PROCESS_NAME="waybar"
+
+# Call the check_process_status script and check exit status immediately
+$CHECK_PROCESS_SCRIPT "$PROCESS_NAME"
+STATUS=$? # Capture exit status immediately
+
+if [ $STATUS -eq 0 ]; then
+  echo -e "Killing waybar\n"
+  pkill waybar
+
+else
+  echo -e "Launching waybar\n"
+  waybar -c ~/.config/waybar/config.json & > /dev/null
+fi
+
